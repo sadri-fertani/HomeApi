1e107fa5a88388c314da6f21e4f3aeaec8c9fb6a
Sadri FERTANI <sadri.fertani@live.fr>
2018-07-29 02:20:02 +0200

    Init Test

---
+++ Waterman.Test/TodoTest.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Net.Http;
+using NUnit.Framework;
+using Waterman.Controllers;
+using Waterman.DataAccess;
+
+namespace Waterman.Test
+{
+    [TestFixture]
+    public class TodoTest
+    {
+        private WatermanContext Context;
+
+        public TodoTest()
+        {
+            this.Context = new WatermanContext(new Microsoft.EntityFrameworkCore.DbContextOptions<WatermanContext>());
+        }
+
+        public TodoTest(WatermanContext context)
+        {
+            this.Context = context;
+        }
+
+        [Test]
+        public void TestGetAll()
+        {
+            // Arrange
+            var ctrl = this.Context.TodoItems;
+
+            // Act
+            var actual = ctrl.Count();
+            var expected = 5;
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
